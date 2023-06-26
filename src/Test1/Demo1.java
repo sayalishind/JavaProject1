dd85797ef359045e152e906cedd906de934e8760
Sayali Shinde <sayalivshinde47@gmail.com>
2023-06-26 17:28:13 +0530

    class file added

---
+++ src/Test1/Demo1.java
@@ -0,0 +1,10 @@
+package Test1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("This is the main method...");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sayal
+ *
+ */
+module JavaProject1 {
+}
